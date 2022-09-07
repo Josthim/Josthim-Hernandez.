99f14f2ef09cbbca68b764a71ee0a7b0bdc3d081
Josthim <110624763+Josthim@users.noreply.github.com>
2022-09-07 12:06:46 -0600

    Add files via upload

---
+++ pjPruebaExtensionMetodos/Program.cs
@@ -0,0 +1,13 @@
+﻿using Modelo;
+
+int numero = 57;
+bool par = numero.EsPar();
+Console.WriteLine("{0} es {1}", numero, par);
+
+// Extensión del double
+double valor = 55.18;
+Console.WriteLine(valor.Duplica());
+
+// Dado un entero realice una extensión de método para saber si es primo 
+bool primo = numero.EsPrimo();
+Console.WriteLine("{0} es {1}", numero, primo);
